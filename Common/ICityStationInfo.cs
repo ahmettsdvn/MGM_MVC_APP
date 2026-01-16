59730eff76456e441cbda31f398d9af440c3fdcd
oguz-taskin <oguzz.taskin@gmail.com>
2026-01-16 22:53:43 +0300

    servis altyapısı hazırlandı

---
+++ Common/ICityStationInfo.cs
@@ -0,0 +1,49 @@
+﻿using Common.Stations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    /// <summary>
+    /// 81 il istasyonlarının bilgilerini tutan interface
+    /// </summary>
+    public interface ICityStationInfo
+    {
+        /// <summary>
+        /// 81 il istasyonlarının bilgilerini döner
+        /// </summary>
+        /// <returns></returns>
+        List<CityStationInfo> GetCityStationInfoList();
+
+        /// <summary>
+        /// Verilen ilin istasyon bilgilerini döner
+        /// </summary>
+        /// <param name="cityName"></param>
+        /// <returns></returns>
+        CityStationInfo GetCityStationInfo(string cityName);
+
+        /// <summary>
+        /// Verilen ilin istasyon bilgilerini döner
+        /// </summary>
+        /// <param name="stationCode"></param>
+        /// <returns></returns>
+        CityStationInfo GetCityStationInfo(int stationCode);
+
+        /// <summary>
+        /// Verilen ilçenin istasyon bilgilerini döner
+        /// </summary>
+        /// <param name="cityName"></param>
+        /// <returns></returns>
+        CityStationInfo GetDistrictStationInfo(string districtName);
+
+        /// <summary>
+        /// Verilen ilçenin istasyon bilgilerini döner
+        /// </summary>
+        /// <param name="stationCode"></param>
+        /// <returns></returns>
+        CityStationInfo GetDistrictStationInfo(int stationCode);
+    }
+}
---
+++ Services/CityStationInfoService.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using Common.Stations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal class CityStationInfoService : ICityStationInfo
+    {
+        public CityStationInfo GetCityStationInfo(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CityStationInfo GetCityStationInfo(int stationCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CityStationInfo> GetCityStationInfoList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CityStationInfo GetDistrictStationInfo(string districtName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CityStationInfo GetDistrictStationInfo(int stationCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
