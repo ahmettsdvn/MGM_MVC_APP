5f81feb7b105237b5d999712c1f7e8c0e4bca86c
oguz-taskin <oguzz.taskin@gmail.com>
2026-01-16 22:31:37 +0300

    Api responce paketleri olusturuldu

---
+++ Common/LastStatus/LastStatusInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.LastStatus
+{
+    /// <summary>
+    /// son durumların bilgilerini tutan sınıf
+    /// </summary>
+    public class LastStatusInfo
+    {
+        public decimal aktuelBasinc { get; set; }
+        public decimal denizSicaklik { get; set; }
+        public decimal denizeIndirgenmisBasinc { get; set; }
+        public int gorus { get; set; }
+        public string hadiseKodu { get; set; }
+        public int istNo { get; set; }
+        public int kapalilik { get; set; }
+        public int karYukseklik { get; set; }
+        public int nem { get; set; }
+        public string rasatMetar { get; set; }
+        public string rasatSinoptik { get; set; }
+        public string rasatTaf { get; set; }
+        public decimal ruzgarHiz { get; set; }
+        public decimal ruzgarYon { get; set; }
+        public decimal sicaklik { get; set; }
+        public DateTime veriZamani { get; set; }
+        public decimal yagis00Now { get; set; }
+        public decimal yagis10Dk { get; set; }
+        public decimal yagis12Saat { get; set; }
+        public decimal yagis1Saat { get; set; }
+        public decimal yagis24Saat { get; set; }
+        public decimal yagis6Saat { get; set; }
+        public DateTime denizVeriZamani { get; set; }
+        public decimal hissedilenSicaklik { get; set; }
+
+                                                         
+        
+        
+       
+        
+     
+       
+       
+          
+    }
+}
