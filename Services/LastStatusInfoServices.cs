b2d0d57233c557cadc8c0d48f75bc4a803516c63
oguz-taskin <oguzz.taskin@gmail.com>
2026-01-16 23:19:31 +0300

    yeni servis ve interface eklendi

---
+++ Services/LastStatusInfoServices.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using Common.LastStatus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal class LastStatusInfoServices : ILastStatusInfo
+    {
+        public LastStatusInfo GetLastStatusInfo(int centerıd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
