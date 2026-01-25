83c867d970cdd14b19f83dd9f9e4297c156d67ca
oguz-taskin <oguzz.taskin@gmail.com>
2026-01-25 14:46:53 +0300

    pagination eklendi

--- MGMMVCAPP/Controllers/HomeController.cs
+++ MGMMVCAPP/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-using System.Diagnostics;
 using Common;
+using Common.DTO;
+using Common.Stations;
 using MGMMVCAPP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using static System.Collections.Specialized.BitVector32;
 
 namespace MGMMVCAPP.Controllers
 {
@@ -10,17 +14,40 @@ namespace MGMMVCAPP.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ICityStationInfo _cityStationInfo;
 
+        private readonly int PAGE_SIZE = 50;
+
         public HomeController(ILogger<HomeController> logger, ICityStationInfo cityStationInfo)
         {
             _logger = logger;
             _cityStationInfo = cityStationInfo;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var allStations = _cityStationInfo.GetAllStations().OrderBy(x => x.il).Take(100).ToList();
+            // page 1'den baþlasýn
+            if (page < 1) page = 1;
 
-            return View(allStations);
+            // toplam kayýt sayýsý
+            int totalCount = _cityStationInfo.GetAllStations().Count;
+
+            // veri alýndý viewa gönderildi
+            var allStations = _cityStationInfo
+                .GetAllStations()
+                .OrderBy(x => x.il)
+                .Skip((page - 1) * PAGE_SIZE)
+                .Take(PAGE_SIZE)
+                .ToList();
+
+            // viev Model DTO
+            var vm = new PagedResult<AllStations>
+            {
+                Items = allStations,
+                Page = page,
+                PageSize = PAGE_SIZE,
+                TotalCount = totalCount
+            };
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
